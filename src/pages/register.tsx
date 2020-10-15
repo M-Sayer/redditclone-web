2fab49ccbb164452d0bce71b445c398eb85cb98f
Muhaajir Benjamin Sayer <muhaajirsayer@Muhaajirs-MBP.fios-router.home>
2020-10-15 13:41:12 -0400

    implemented email for login, register, nodemailer to send emails

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -20,9 +20,9 @@ const Register: React.FC<registerProps> = ({}) => {
   return (
     <Wrapper variant='small' >
       <Formik 
-        initialValues={{ username: '', password: ''}}
+        initialValues={{ email: '', username: '', password: ''}}
         onSubmit={async (values, {setErrors}) => {
-          const response = await register(values);
+          const response = await register({ options: values });
           if (response.data?.register.errors) {
             setErrors(toErrorMap(response.data.register.errors))
           } else if (response.data?.register.user) router.push('/');
@@ -34,12 +34,16 @@ const Register: React.FC<registerProps> = ({}) => {
               placeholder='username' 
               label='Username' 
             />
-            <Box mt={4} />
-            <InputField name='password' 
-              placeholder='password' 
-              label='Password' 
-              type='password'
-            />
+            <Box mt={4}>
+              <InputField name='email' placeholder='email' label='Email' />
+            </Box>
+            <Box mt={4}>
+              <InputField name='password' 
+                placeholder='password' 
+                label='Password' 
+                type='password'
+              />
+            </Box>
             <Button mt={4} 
               type='submit' 
               isLoading={isSubmitting} 
